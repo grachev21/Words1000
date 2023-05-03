185d113b60e2fa6b50f60447721af40bd105ec9b
grachev21 <grachev613@gmail.com>
2023-05-03 14:53:33 +0400

    link_url

--- words/models.py
+++ words/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+
+class Word_status(models.Model):
+    status = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.status
+
 class WordsCard(models.Model):
     word_en = models.TextField(blank=True)
     transcription = models.TextField(blank=True)
@@ -7,6 +14,7 @@ class WordsCard(models.Model):
     phraze_en = models.TextField(blank=True)
     phraze_ru = models.TextField(blank=True)
     number_repetitions = models.TextField(blank=True)
+    word_status = models.ForeignKey(Word_status, on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.word_en
--- words/views.py
+++ words/views.py
@@ -4,11 +4,23 @@ from django.http import HttpResponseNotFound
 from .models import WordsCard
 
 
-menu = ['Главная',
-        'Учить навые слова',
-        'Повторить выученные сегодня',
-        'Повторить последние 50',
-        'Выход']
+menu = [
+        {'title': 'Главная',
+        'url_name': 'home'},
+
+        {'title': 'Учить новые слова',
+        'url_name': 'learn_new_words'},
+
+        {'title': 'Повторить выученные сегодня',
+        'url_name': 'revise_learned_today'},
+
+        {'title': 'Повторить последние 50',
+        'url_name': 'repeat_last_50'},
+
+        {'title': 'Выход',
+        'url_name': 'out'}
+        ]
+
 
 
 def home(request):
@@ -20,6 +32,34 @@ def home(request):
             }
     return render(request, 'words/home.html', context=context)
 
+def learn_new_words(request):
+    context = {
+            'menu': menu,
+            'title': 'Учить новые слова'
+            }
+    return render(request, 'words/learn_new_words.html', context=context)
+
+
+def revise_learned_today(request):
+    context = {
+            'menu': menu,
+            'title': 'Повторить выучинные сегодня'
+            }
+    return render(request, 'words/revise_learned_today.html', context=context)
+
+def repeat_last_50(request):
+    context = {
+            'menu': menu,
+            'title': 'Повторить последнии 50'
+            }
+    return render(request, 'words/repeat_last_50.html', context=context)
+
+def out(request):
+    context = {
+            'menu': menu,
+            'title': 'Выход'
+            }
+    return render(request, 'words/out.html', context=context)
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('Страничка не найдена')
