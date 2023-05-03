b990d8749c75169e0b25f46e90b19b8de4882f62
grachev21 <grachev613@gmail.com>
2023-05-03 18:56:29 +0400

    admin_panel

--- words/admin.py
+++ words/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import WordsCard
+from .models import Word_status
+from .models import Word_Accumulator
+
+
+class WordStatusAdmin(admin.ModelAdmin):
+    list_display = ('status',)
+
+
+class WordsCardAdmin(admin.ModelAdmin):
+    list_display = (
+                    'word_en',
+                    'transcription',
+                    'word_ru',
+                    'phrases_en',
+                    'phrases_ru',
+                    )
+
+class Word_AccumulatorAdmin(admin.ModelAdmin):
+    list_display = ('word',)
+
+
+admin.site.register(Word_status, WordStatusAdmin)
+admin.site.register(WordsCard, WordsCardAdmin)
+admin.site.register(Word_Accumulator, Word_AccumulatorAdmin)
+
--- words/models.py
+++ words/models.py
@@ -2,19 +2,37 @@ from django.db import models
 
 
 class Word_status(models.Model):
-    status = models.TextField(blank=True)
+    status = models.TextField(blank=True, verbose_name='Статус слова')
 
     def __str__(self):
         return self.status
 
+    class Meta:
+        verbose_name = 'Статус слова'
+        verbose_name_plural = 'Статус слов'
+
 class WordsCard(models.Model):
-    word_en = models.TextField(blank=True)
-    transcription = models.TextField(blank=True)
-    word_ru = models.TextField(blank=True)
-    phraze_en = models.TextField(blank=True)
-    phraze_ru = models.TextField(blank=True)
-    number_repetitions = models.TextField(blank=True)
-    word_status = models.ForeignKey(Word_status, on_delete=models.PROTECT, null=True)
+    word_en = models.CharField(blank=True, max_length=100, verbose_name='На аглийском')
+    transcription = models.CharField(blank=True, max_length=100, verbose_name='Транскрипция')
+    word_ru = models.CharField(blank=True, max_length=100, verbose_name='На русском')
+    phrases_en = models.TextField(blank=True, verbose_name='Фразы на английском')
+    phrases_ru = models.TextField(blank=True, verbose_name='Фразы на русском')
+
+    class Meta:
+        verbose_name = 'Слово'
+        verbose_name_plural = 'Слова'
+
 
     def __str__(self):
         return self.word_en
+
+class Word_Accumulator(models.Model):
+    word = models.CharField(max_length=100, verbose_name='Слово')
+    number_repetitions = models.TextField(blank=True, verbose_name='Количество повторений')
+    word_status = models.ForeignKey(Word_status, on_delete=models.PROTECT, blank=True, verbose_name='Статус слова')
+
+    class Meta:
+        verbose_name = 'Накопитель слов'
+        verbose_name_plural = 'Накопитель слов'
+
+
