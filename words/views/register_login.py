0c27fe3e75d974bac6bf801842346e0e472d2f35
grachev21 <grachev613gmail.com>
2023-11-12 09:55:33 +0000

    edit error

--- words/views/register_login.py
+++ words/views/register_login.py
@@ -6,6 +6,7 @@ from django.shortcuts import redirect
 from ..models import *
 from ..forms import *
 from ..separate_logic.views_logic import DataMixin
+from django.contrib.auth import get_user_model
 
 
 # class RegisterUser(DataMixin, CreateView):
