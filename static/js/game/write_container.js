bfed97e12855bf6c656c5c3e8acb563d79364f00
grachev21 <grachev613@gmail.com>
2025-11-28 09:51:45 +0300

    fix game/write_word

--- static/js/game/write_container.js
+++ static/js/game/write_container.js
@@ -1,47 +1,101 @@
 class WriteContainer {
     constructor() {
-        // this. settings = JSON.parse(document.getElementById("settings-data").textContent)
-        this.input = document.getElementById("input-text-en");
+        this.max_number_read = this.getSettings().max_number_read;
+        this.inputEn = document.getElementById("input-text-en");
+        this.inputRu = document.getElementById("input-text-ru");
+        this.en = false;
+        this.blackout = document.getElementById("screen-blackout");
+        this.counter = document.getElementById("counter-write");
         this.init();
     }
 
     init() {
         this.changeColorError();
+        this.counter.textContent = this.max_number_read;
     }
 
-    // sessionStorageData(){
-    //     let data = JSON.parse(document.getElementById("game-data").textContent);
-    // }
+    getSettings() {
+        return JSON.parse(document.getElementById("user-settings").textContent);
+    }
+
+    errorColor() {
+        this.inputEn.classList.replace("text-col_suc_ave", "text-col_attn");
+        this.inputEn.classList.replace("text-col_con", "text-col_attn");
+    }
+    normalColor() {
+        this.inputEn.classList.replace("text-col_attn", "text-col_suc_ave");
+        this.inputEn.classList.replace("text-col_con", "text-col_suc_ave");
+    }
+    executionColor() {
+        this.inputEn.classList.replace("text-col_attn", "text-col_con");
+        this.inputEn.classList.replace("text-col_suc_ave", "text-col_con");
+    }
+    moveNextWord() {
+        this.blackout.value = this.max_number_read;
+        if (!this.max_number_read <= 0) {
+            this.max_number_read--;
+            this.blackout.querySelector("div").textContent =
+                this.max_number_read;
+            this.blackout.classList.remove("invisible");
+            this.counter.textContent = this.max_number_read;
+            setTimeout(() => {
+                this.inputEn.value = "";
+                this.blackout.classList.add("invisible");
+            }, 1000);
+        }
+    }
+
+    wordSwithc() {
+        if (!this.en) {
+            this.en = true;
+            this.inputRu.classList.remove("hidden");
+            this.inputEn.classList.add("hidden");
+        }
+    }
     changeColorError() {
-        this.input.addEventListener("input", (e) => {
+        this.inputEn.addEventListener("input", (e) => {
             this.strInput = e.target.value;
 
             if (
                 this.strInput !==
-                this.input.dataset.word.slice(0, this.strInput.length)
+                this.inputEn.dataset.word.slice(0, this.strInput.length)
             ) {
-                this.input.classList.replace(
-                    "text-col_suc_ave",
-                    "text-col_attn",
-                );
-                this.input.classList.replace("text-col_con", "text-col_attn");
+                this.errorColor();
             } else if (this.strInput.length == 0) {
-                this.input.classList.replace(
-                    "text-col_attn",
-                    "text-col_suc_ave",
-                );
-                this.input.classList.replace(
-                    "text-col_con",
-                    "text-col_suc_ave",
-                );
+                this.normalColor();
+            } else if (this.strInput == this.inputEn.dataset.word) {
+                this.wordSwithc();
+                // this.moveNextWord();
             } else {
-                this.input.classList.replace("text-col_attn", "text-col_con");
-                this.input.classList.replace(
-                    "text-col_suc_ave",
-                    "text-col_con",
-                );
+                this.executionColor();
+            }
+        });
+
+        this.inputRu.addEventListener("input", (e) => {
+            this.strInput = e.target.value;
+            console.log(this.strInput);
+            console.log(
+                this.strInput,
+                this.inputRu.dataset.word.slice(0, this.strInput.length),
+            );
+
+            if (
+                this.strInput !==
+                this.inputRu.dataset.word.slice(0, this.strInput.length)
+            ) {
+                this.errorColor();
+            } else if (this.strInput.length == 0) {
+                this.normalColor();
+            } else if (this.strInput == this.inputRu.dataset.word) {
+                this.moveNextWord();
+            } else {
+                this.executionColor();
             }
         });
     }
+
+    // settionStorage(){
+    //     sessionStorage.sessionStorage("number": )
+    // }
 }
 export default WriteContainer;
